7c62ed9c5eca0c8489e5ff07955224e7d78c98f6
Bartoszd (bartek3x) <164915721+Bartosz8291@users.noreply.github.com>
2025-06-23 21:02:50 +0200

    Bua 1.0.0 Files

---
+++ Bua_ProgrammingLanguage/Bua.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Bua_ProgrammingLanguage.Bua_Assets;
+
+namespace Bua_ProgrammingLanguage
+{
+    public static class Bua
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Bua 1.0.0 32-bit");
+                Console.WriteLine("Type \"help\" for a list of avaliable commands.");
+
+                while (true)
+                {
+                    Console.Write("> ");
+                    var command = Console.ReadLine();
+                    Commands.RunCommand(command);
+                }
+            }
+            else
+            {
+                string filePath = args[0];
+
+                if (!filePath.EndsWith(".bua", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("[Error] ");
+                    Console.ResetColor();
+                    Console.WriteLine("Only .bua files are accepted.");
+                    return;
+                }
+
+                if (File.Exists(filePath))
+                {
+                    var code = File.ReadAllText(filePath);
+                    var lines = code.Split(new[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+                    foreach (var line in lines)
+                    {
+                        Commands.RunCommand(line.Trim());
+                    }
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("[Error] ");
+                    Console.ResetColor();
+                    Console.WriteLine("File does not exist.");
+                }
+            }
+        }
+    }
+}
---
+++ Bua_ProgrammingLanguage/Bua_Assets/Commands.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bua_ProgrammingLanguage.Bua_Assets
+{
+    public static class Commands
+    {
+        public static void RunCommand(string command)
+        {
+            command = command.Trim();
+
+            string[] cmd = command.Split(' ');
+
+            if(cmd[0] == "")
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("[Error] ");
+                Console.ResetColor();
+                Console.WriteLine("Command is Empty.");
+            }
+            else if(cmd[0] == "help")
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("List of avaliable commands:");
+                Console.ResetColor();
+                ShowCommand("help", "Show this message");
+                ShowCommand("exit", "Exit Bua interpeter");
+                ShowCommand("clear", "Clears the console");
+                ShowCommand("print <text>", "Prints text");
+            }
+            else if (cmd[0] == "exit")
+            {
+                Environment.Exit(0);
+            }
+            else if (cmd[0] == "clear")
+            {
+                Console.Clear();
+            }
+            else if (cmd[0] == "print")
+            {
+                if (cmd.Length < 2)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("[Error] ");
+                    Console.ResetColor();
+                    Console.WriteLine("print requires an argument.");
+                }
+                else
+                {
+                    Console.WriteLine(string.Join(" ", cmd.Skip(1)));
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("[Error] ");
+                Console.ResetColor();
+                Console.WriteLine("Invalid Command");
+            }
+        }
+        private static void ShowCommand(string name, string description)
+        {
+            Console.WriteLine($"{name} -- {description}");
+        }
+    }
+}
